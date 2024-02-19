c044bb1691498d15fdc985f3295c441dfbbae2e7
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2024-02-19 22:26:56 +0530

    Create Pascal-triangles.cpp

---
+++ Pascal-triangles.cpp
@@ -0,0 +1,32 @@
+Using formula to solve the question ->  R-1
+                                          C
+                                           C-1
+  where R = number of rows
+C = number of columns
+
+class Solution {
+public:
+int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return (int)(res);
+}
+    vector<vector<int>> generate(int n) {
+         vector<vector<int>> ans;
+
+    //Store the entire pascal's triangle:
+    for (int row = 1; row <= n; row++) {
+        vector<int> tempLst; // temporary list
+        for (int col = 1; col <= row; col++) {
+            tempLst.push_back(nCr(row - 1, col - 1));
+        }
+        ans.push_back(tempLst);
+    }
+    return ans;
+    }
+};
